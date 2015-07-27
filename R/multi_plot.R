d7f5b4da2492c5c110151d1cf06a13607236911b
evpickett <liquid.ghoul@gmail.com>
2015-07-27 16:13:27 +0800

    Added new function multi_plot for multiple plots on one page

---
+++ R/multi_plot.R
@@ -0,0 +1,46 @@
+#' multi_plot
+#'
+#' ggplot objects can be passed into this function to create multiple plots on one page. Created by www.cookbook-r.com
+#' @param cols Number of columns in layout
+#' @param layout A matrix specifying the layout. If present, 'cols' is ignored 
+#' @param plotlist A list of plots as opposed to the series
+#' @keywords ggplot2 multiple plot
+#' @export
+#' @examples
+#' multiplot(p1, p2, p3, p4, cols=2)
+
+multi_plot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
